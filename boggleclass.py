691e1d35b9023754d6b057c40c4a8ffaeed0d7f9
Revant Kantamneni <rkantamneni20@choate.edu>
2017-12-15 12:54:45 -0500

    hi
     hi

--- boggleclass.py
+++ boggleclass.py
@@ -1,22 +1,23 @@
 import random
 import sys
-
+import numpy as py4
 class gui_board:
 	def __init__(self, master):
 		self.master = master
 		self.array
 		self.letters =
-		['L', 'R', 'E', "V", 'D', "Y"]['R', 'N', 'Z', 'N', 'H', 'L']['E', 'D', 'L', 'X', 'R', 'I']['G','A','E','A','E','N']['QU', 'N', 'M', 'I', 'U', 'H']
+		[['L', 'R', 'E', "V", 'D', "Y"]['R', 'N', 'Z', 'N', 'H', 'L']['E', 'D', 'L', 'X', 'R', 'I']['G','A','E','A','E','N']['QU', 'N', 'M', 'I', 'U', 'H']
 		['D', 'S', 'Y', 'I', 'T', 'T']['I', 'T', 'S', 'E', 'S', 'O']['S', 'N', 'E', 'I', 'E', 'U']['R', 'Y', 'T', 'L', 'T', 'E']['A', 'S', 'P', 'F'. 'K', 'F']
 		['W', 'E', 'G', 'H', 'H', 'E']['R', 'V', 'T', 'H', 'E', 'W']['C', 'I', 'U', 'T', 'M', 'O']['S','C','A','O','P','H']['B', 'A', 'O', 'B', 'O', 'J']
-		['A', 'O', 'T', 'T', 'W', 'O']
+		['A', 'O', 'T', 'T', 'W', 'O']]
 		self.board = [[][][][]]
 class dice:
 	def __init__():
 
 	def assign_values(self):
 		for i in range(15):
-			self.board[.append(random(self.letters[i]))
+			#Pick random array, pick random element, delete element while keeping order
+			self.board[0][i].append(random(self.letters[i]))
 class points:
 	def __init__():
 
@@ -24,3 +25,4 @@ class timer:
 	def __init__(self, time):
 		self.time = time
 	def duration(self, time):
+x = numpy.delete(x, (0))

d7be0b3e24e9b060c98216a8ce7cf785587d9f68
wwildridge19 <34096939+wwildridge19@users.noreply.github.com>
2017-12-19 17:18:11 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,9 +5,17 @@ import numpy as py4
 import time
 from tkinter import *
 
-x.gui_board()
-x.dice()
-x.timer() 
-x.check()
-x.scoring()
-x.end_game()
+play_game = input('would you like to play? (y/n)')
+while play_game = 'y':
+  x.gui_board()
+  x.dice()
+  x.timer() 
+  x.check()
+  x.scoring()
+  x.end_game()
+  play_game = input('would you like to play again? (y/n)')
+if play_game = 'n':
+  print: 'ok thank you'
+ playgame = input('please type y for yes or n for no') 
+
+

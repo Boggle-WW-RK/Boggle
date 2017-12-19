3166881d22a69af5624a7971d4aae8b34428de88
wwildridge19 <34096939+wwildridge19@users.noreply.github.com>
2017-12-19 17:12:52 -0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,1 +1,13 @@
+from boggleclass import gui_board, dice, timer, scoring, end_game, check
+import random
+import sys
+import numpy as py4
+import time
+from tkinter import *
 
+x.gui_board()
+x.dice()
+x.timer() 
+x.check()
+x.scoring()
+x.end_game()
